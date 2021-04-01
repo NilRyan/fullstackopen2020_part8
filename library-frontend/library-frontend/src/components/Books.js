656531980bf3465dbbbe89916f36e5d397e822b0
Neil Ryan <neilryanlipaod@gmail.com>
2021-04-01 11:40:01 +0800

    8.19 Books by genre, part 1
    ADDED a filter in the Books component to display books with specific
    genres.

--- library-frontend/library-frontend/src/components/Books.js
+++ library-frontend/library-frontend/src/components/Books.js
@@ -1,10 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { ALL_BOOKS } from '../queries'
 import { useQuery } from '@apollo/client'
 
 const Books = (props) => {
   const {loading, data} = useQuery(ALL_BOOKS)
-
+  const [showGenre, setShowGenre] = useState('ALL')
   if (!props.show) {
     return null
   }
@@ -18,31 +18,44 @@ const Books = (props) => {
   }
 
   const books = [...data.allBooks]
+  const allGenres = ['ALL', ...new Set (books.map((book) => book.genres).flat())]
+  
+  console.log(allGenres)
 
   return (
     <div>
       <h2>books</h2>
-
+      <h1>in genre {showGenre} </h1>
+    <div>
       <table>
-        <tbody>
-          <tr>
-            <th></th>
-            <th>
-              author
-            </th>
-            <th>
-              published
-            </th>
-          </tr>
-          {books.map(a =>
-            <tr key={a.id}>
-              <td>{a.title}</td>
-              <td>{a.author.name}</td>
-              <td>{a.published}</td>
+          <tbody>
+            <tr>
+              <th></th>
+              <th>
+                author
+              </th>
+              <th>
+                published
+              </th>
             </tr>
-          )}
-        </tbody>
-      </table>
+            
+            {books.filter((book) => showGenre === 'ALL' ? true : book.genres.includes(showGenre))
+              .map(a =>
+              <tr key={a.id}>
+                <td>{a.title}</td>
+                <td>{a.author.name}</td>
+                <td>{a.published}</td>
+              </tr>
+            )}
+          </tbody>
+        </table>
+    </div>
+      {
+        allGenres.map((genre) => <button key={genre} onClick={() => setShowGenre(genre)}>{genre}</button>)
+      }
+    <div>
+      
+    </div>
     </div>
   )
 }
